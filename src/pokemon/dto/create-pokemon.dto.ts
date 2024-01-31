066009f7e6c288097668b1035354e07f6c175ffc
Luis Cereño <luis.cereno.t@gmail.com>
2024-01-31 20:11:23 -0300

    Se crea funcion para crear pokemon y guardar en la BD

--- src/pokemon/dto/create-pokemon.dto.ts
+++ src/pokemon/dto/create-pokemon.dto.ts
@@ -1,1 +1,13 @@
-export class CreatePokemonDto {}
+import { IsInt, IsPositive, IsString, Min, MinLength } from "class-validator";
+
+export class CreatePokemonDto {
+    
+    @IsString()
+    @MinLength(1)
+    name: string;
+    
+    @IsInt()
+    @IsPositive()
+    @Min(1)
+    no: number;
+}
--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -1,11 +1,22 @@
 import { Injectable } from '@nestjs/common';
 import { CreatePokemonDto } from './dto/create-pokemon.dto';
 import { UpdatePokemonDto } from './dto/update-pokemon.dto';
+import { Model } from 'mongoose';
+import { Pokemon } from './entities/pokemon.entity';
+import { InjectModel } from '@nestjs/mongoose';
 
 @Injectable()
 export class PokemonService {
-  create(createPokemonDto: CreatePokemonDto) {
-    return 'This action adds a new pokemon';
+  constructor(
+    @InjectModel( Pokemon.name )
+    private readonly pokemonModel: Model<Pokemon>
+  ) {}
+
+  async create( createPokemonDto: CreatePokemonDto ) {
+    createPokemonDto.name = createPokemonDto.name.toLocaleLowerCase();
+    const pokemon = await this.pokemonModel.create( createPokemonDto );
+
+    return pokemon;
   }
 
   findAll() {
