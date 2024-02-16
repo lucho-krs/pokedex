6fa73693f01a44430027194952dadc1cd82ccf85
Luis Cereño <luis.cereno.t@gmail.com>
2024-02-16 00:04:04 -0300

    Se crea SEED para llamda de ppokeapi

---
+++ src/seed/seed.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@nestjs/common';
+import axios, { AxiosInstance } from 'axios';
+import { PokeResponse } from './interfaces/poke-response.interfaces';
+
+@Injectable()
+export class SeedService {
+  private readonly axios: AxiosInstance = axios;
+  
+  async executeSeed() {
+    const { data } = await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=10');
+
+    data.results.forEach(({ name, url }) => {
+      const segments = url.split('/');
+      const no = +segments[ segments.length - 2 ];
+
+      console.log({ no, name});
+    });
+
+    return data.results;
+  } 
+}
